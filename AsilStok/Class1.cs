42a0ececfdfe9ccecb9cb041f270d3b347fbefeb
EMRE5 <EMRE5@EB-LAPTOP>
2022-03-22 17:23:26 +0300

    Add project files.

---
+++ AsilStok/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsilStok
+{
+    public class Stock
+    {
+        String tip;
+        decimal price;
+        int amount = 10;
+        public Stock(String tip) => this.tip = tip;
+
+        public delegate void EHandler();
+        public event EHandler PriceChanced;
+
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                PriceChanced?.Invoke();// olay tetiklendi
+            }
+        }
+
+
+
+    }
+
+
+    public class PriceChancedEventArgs : EventArgs
+    {
+        public readonly decimal oldPrice;
+        public readonly decimal newPrice;
+        public PriceChancedEventArgs(decimal oldPrice, decimal newPrice)
+        {
+            this.oldPrice = oldPrice;
+            this.newPrice = newPrice;
+        }
+    }
+}
